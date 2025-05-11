8f91a1d15bc7c78c69386c952e595494e20a48ae
chemsodev <bourabiachemseddine@gmail.com>
2025-05-11 06:07:49 +0100

    fixes

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 import { Notification } from './notification.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Notification])],
+    imports: [
+        ConfigModule,
+        TypeOrmModule.forFeature([Notification])
+    ],
     controllers: [NotificationsController],
     providers: [NotificationsService],
     exports: [NotificationsService]
