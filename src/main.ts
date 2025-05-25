06b63289d9a914a4a1b47eae5f87935d18cddcaf
chems eddine bourabia <bourabiachemseddine@gmail.COM>
2025-05-25 07:49:41 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,7 @@ async function bootstrap() {
       "http://localhost:3001",
       "http://localhost:8080",
       "http://127.0.0.1:8080",
+      "https://uni-front-zeta.vercel.app",
       "null", // For file:// protocol
       "*", // Allow all origins during development
     ],
